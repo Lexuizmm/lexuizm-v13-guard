f505f15ca4aac0d84d4564c19fbd0a15dd616c04
Lexuizm Tanrıverdi <150593266+Lexuizmm@users.noreply.github.com>
2024-01-30 22:58:51 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 module.exports = {
-    ShieldI:"OTY1NTE1Nzk0OTIwMzkwNzI2.G_xHbL.78XJG-8pH9rVYJgHQa770Y9YomiFPKf1BJy5mE",
-    Shield2:"OTY4NjMyMDU0MTc2Njk0Mzcy.GAZwO6.ZhrfaTi6ZhAQqJReEz34rgE0TRBy2ZTiOxlDl4",
-    guildID:"1102170844081438810",
-    Mongoose:"mongodb+srv://karamguard123:karamguard123@cluster0.68lxxl5.mongodb.net/shewnlexuizm?retryWrites=true&w=majority",
-    Owner:["693185687758962698"],
+    ShieldI:"",
+    Shield2:"",
+    guildID:"",
+    Mongoose:"",
+    Owner:[""],
     SesKanalıAdı: "BotVoice",
     Durum:["Shewn ❤ Lexuizm"],
     Prefixs:["."]
